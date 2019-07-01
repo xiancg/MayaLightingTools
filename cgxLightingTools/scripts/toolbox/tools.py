bef224ee1d5d2e75ea870011bf6f0c6c0c76276d
Chris Granados- Xian <chris.granados@xiancg.com>
2019-07-01 16:52:57 -0300

    Added simpleIsolateLight and tests

--- cgxLightingTools/scripts/toolbox/tools.py
+++ cgxLightingTools/scripts/toolbox/tools.py
@@ -8,6 +8,7 @@ TODO: Build attributes dict from light nodes for lightsManager
 '''
 import maya.cmds as mc
 
+lightsOff = list()
 
 def getRenderEngines():
     allPlugins = mc.pluginInfo(query=True, listPlugins=True)
@@ -21,11 +22,12 @@ def getRenderEngines():
         return None
     return renderEngines
 
+
 def getLightNodes():
-    lightNodes = mc.listNodeTypes( 'drawdb/light' )
+    lightNodes = mc.listNodeTypes('drawdb/light')
     renderEngines = getRenderEngines()
     result = dict()
-    for key,value in renderEngines.iteritems():
+    for key, value in renderEngines.iteritems():
         dependNodes = mc.pluginInfo(key, query=True, dependNode=True)
         rendererLights = list(set(lightNodes).intersection(dependNodes))
         result[key] = rendererLights
@@ -36,15 +38,70 @@ def getLightNodes():
 
     return result
 
+
+def getLightNodesList():
+    lightNodesDict = getLightNodes()
+    lightNodes = list()
+    for each in lightNodesDict.itervalues():
+        lightNodes += each
+    return lightNodes
+
+
 def getDefaultLightNodes():
     return getLightNodes()['default']
+
 
 def getRendererLightNodes(renderer='default'):
     if mc.pluginInfo(renderer, query=True, loaded=True) or renderer == 'default':
         return getLightNodes()[renderer]
     else:
-        raise ValueError("No renderer named '{}' was found loaded.".format(renderer))
+        raise ValueError(
+            "No renderer named '{}' was found loaded.".format(renderer))
         return None
+
+
+def simpleIsolateLights():
+    lightNodes = getLightNodesList()
+    # All scene lights
+    allLightsShapes = mc.ls(lights=True, type=lightNodes, long=True)
+    allLightsScene = []
+    for i in allLightsShapes:
+        iTrans = mc.listRelatives(i, parent=True, fullPath=True)
+        allLightsScene.append(iTrans[0])
+
+    # Selected scene lights
+    selObjs = mc.ls(sl=True, long=True)
+    selLights = []
+    for e in selObjs:
+        eShape = mc.listRelatives(
+            e, shapes=True, noIntermediate=True, fullPath=True)
+        eNodeType = mc.nodeType(eShape[0])
+        if eNodeType in lightNodes:
+            selLights.append(e)
+
+    # Toggle visibilities
+    for light in allLightsScene:
+        # If light is ON but not selected, switch it OFF
+        if mc.getAttr(light + ".visibility") == True and light not in selLights:
+            mc.setAttr((light + ".visibility"), False)
+        #Else, if light is OFF and was not OFF before this command, switch it ON
+        elif mc.getAttr(light + ".visibility") == False and light not in lightsOff:
+            mc.setAttr((light + ".visibility"), True)
+
+
+def storeLightsOffStatus():
+    allLightsScene = list()
+    del lightsOff[:]
+    lightNodes = getLightNodesList()
+    allLightsShapes = mc.ls(lights=True, type=lightNodes, long=True)
+    for i in allLightsShapes:
+        iTrans = mc.listRelatives(i, parent=True, fullPath=True)[0]
+        allLightsScene.append(iTrans)
+    for light in allLightsScene:
+        if mc.getAttr(light + ".visibility") == False:
+            lightsOff.append(light)
+
+storeLightsOffStatus()
 
 def main():
     print getRenderEngines()
--- cgxLightingTools/scripts/toolbox/tools_test.py
+++ cgxLightingTools/scripts/toolbox/tools_test.py
@@ -21,21 +21,56 @@ class NodesTest(unittest.TestCase):
 
     def test_getLightNodes(self):
         lightNodes = tools.getLightNodes()
-        defaultLightNodes = ['ambientLight', 'areaLight', 'directionalLight', 
-                            'pointLight', 'spotLight', 'volumeLight']
-        self.assertItemsEqual
-    
+        defaultLightNodes = ['ambientLight', 'areaLight', 'directionalLight',
+                             'pointLight', 'spotLight', 'volumeLight']
+        self.assertItemsEqual(defaultLightNodes, lightNodes['default'])
+
+    def test_getLightNodesList(self):
+        lightNodes = tools.getLightNodesList()
+        self.assertGreaterEqual(lightNodes, 6)
+
     def test_getDefaultLightNodes(self):
         lightNodes = tools.getDefaultLightNodes()
-        defaultLightNodes = ['ambientLight', 'areaLight', 'directionalLight', 
-                            'pointLight', 'spotLight', 'volumeLight']
+        defaultLightNodes = ['ambientLight', 'areaLight', 'directionalLight',
+                             'pointLight', 'spotLight', 'volumeLight']
         self.assertItemsEqual(defaultLightNodes, lightNodes)
 
     def test_getLightNodesForRenderer(self):
         lightNodes = tools.getRendererLightNodes('mtoa')
-        arnoldLightNodes = ['aiPhotometricLight','aiMeshLight','aiSkyDomeLight',
-                            'aiSky','aiAreaLight']
+        arnoldLightNodes = ['aiPhotometricLight', 'aiMeshLight', 'aiSkyDomeLight',
+                            'aiSky', 'aiAreaLight']
         self.assertItemsEqual(lightNodes, arnoldLightNodes)
+
+
+class FunctionsTest(unittest.TestCase):
+    def setUp(self):
+        light1 = mc.spotLight()
+        light2 = mc.spotLight()
+        light3 = mc.spotLight()
+        light4 = mc.spotLight()
+        light5 = mc.spotLight()
+
+        self.light1Trans = mc.listRelatives(light1, parent=True, fullPath=True)[0]
+        self.light2Trans = mc.listRelatives(light2, parent=True, fullPath=True)[0]
+        self.light3Trans = mc.listRelatives(light3, parent=True, fullPath=True)[0]
+        self.light4Trans = mc.listRelatives(light4, parent=True, fullPath=True)[0]
+        self.light5Trans = mc.listRelatives(light5, parent=True, fullPath=True)[0]
+
+        mc.setAttr(self.light4Trans + '.visibility', 0)
+        mc.setAttr(self.light5Trans + '.visibility', 0)
+        tools.storeLightsOffStatus()
+
+    def test_simpleIsolateLights(self):
+        mc.select(self.light1Trans,replace=True)
+        tools.simpleIsolateLights()
+        self.assertTrue(mc.getAttr(self.light1Trans + '.visibility'))
+        self.assertFalse(mc.getAttr(self.light2Trans + '.visibility'))
+        self.assertFalse(mc.getAttr(self.light4Trans + '.visibility'))
+
+        tools.simpleIsolateLights()
+        self.assertTrue(mc.getAttr(self.light1Trans + '.visibility'))
+        self.assertTrue(mc.getAttr(self.light2Trans + '.visibility'))
+        self.assertFalse(mc.getAttr(self.light4Trans + '.visibility'))
 
 
 if __name__ == "__main__" or 'eclipsePython' in __name__:
