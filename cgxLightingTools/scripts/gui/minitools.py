31f99fcac79f4688c15cfa2820f0f260364f70cf
Chris Granados- Xian <chris.granados@xiancg.com>
2019-07-15 15:43:46 -0300

    A bit of code refactoring

--- cgxLightingTools/scripts/gui/minitools.py
+++ cgxLightingTools/scripts/gui/minitools.py
@@ -3,7 +3,6 @@ Created on July 10, 2019
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
-TODO: Vertical layout not working
 TODO: Set width and height sizing properties on show depending on preferences
 '''
 from __future__ import absolute_import
@@ -19,13 +18,14 @@ import cgxLightingTools.scripts.toolbox.lightsFactory as lightsFactory
 class MiniToolsDialog(QtWidgets.QDialog):
     def __init__(self, parent=mWin.getMayaWindow()):
         super(MiniToolsDialog, self).__init__(parent=parent)
+        self._prefOrientation = 'horizontal'
         self.initFactories()
         self.setupUi()
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
     
     def initFactories(self):
         '''TODO: Traverse render engines and create factories accordingly.
-        Also list factories and use what is available'''
+        Also list factories, compare and use what is available'''
         renderers = tools.getRenderEngines()
         self.defaultFactory = lightsFactory.default_factory.LightsFactory()
         if renderers is not None:
@@ -49,9 +49,7 @@ class MiniToolsDialog(QtWidgets.QDialog):
         self.setWindowTitle('Mini Tools')
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
-        
-        pref= 'horizontal'
-        if pref == 'horizontal':
+        if self._prefOrientation == 'horizontal':
             self.resize(392, 90)
             self.setMinimumSize(QtCore.QSize(392,90))
             self.setMaximumSize(QtCore.QSize(10000, 90))
@@ -63,11 +61,123 @@ class MiniToolsDialog(QtWidgets.QDialog):
             mainLayout = self.loadVertical()
         tools_GRIDLAY = QtWidgets.QGridLayout()
         tools_GRIDLAY.setObjectName("tools_GRIDLAY")
+        lgtVis_GRIDLAY = QtWidgets.QGridLayout()
+        lgtVis_GRIDLAY.setObjectName("lgtVis_GRIDLAY")
+        defaultLights_GRIDLAY = QtWidgets.QGridLayout()
+        defaultLights_GRIDLAY.setObjectName("defaultLights_GRIDLAY")
+        rendererLights_GRIDLAY = QtWidgets.QGridLayout()
+        rendererLights_GRIDLAY.setObjectName("rendererLights_GRIDLAY")
+        self.createButtons()
+        if mainLayout.objectName() == 'horizontalLayout':
+            tools_GRIDLAY.addWidget(self.simpleIsolate_BTN, 0, 0, 1, 1)
+            tools_GRIDLAY.addWidget(self.lookThru_BTN, 0, 1, 1, 1)
+            tools_GRIDLAY.addWidget(self.aimLight_BTN, 0, 2, 1, 1)
+            tools_GRIDLAY.addWidget(self.specularConstrain_BTN, 0, 3, 2, 1)
+            tools_GRIDLAY.addWidget(self.cleanUpCams_BTN, 1, 0, 1, 1)
+            tools_GRIDLAY.addWidget(self.lightsManager_BTN, 1, 1, 1, 1)
+            tools_GRIDLAY.addWidget(self.alignLight_BTN, 1, 2, 1, 1)
+            mainLayout.addLayout(tools_GRIDLAY)
+            spacerWidth = 8
+            spacerHeight = 20
+            spacerItem1 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight, 
+                                QtWidgets.QSizePolicy.Expanding,
+                                QtWidgets.QSizePolicy.Minimum)
+            mainLayout.addItem(spacerItem1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot01_BTN, 0, 0, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot02_BTN, 0, 1, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot03_BTN, 2, 1, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot04_BTN, 1, 0, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot05_BTN, 1, 1, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot06_BTN, 2, 0, 1, 1)
+            mainLayout.addLayout(lgtVis_GRIDLAY)
+            spacerItem2 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight, 
+                                        QtWidgets.QSizePolicy.Expanding,
+                                        QtWidgets.QSizePolicy.Minimum)
+            mainLayout.addItem(spacerItem2)
+            lights_VERTLAY = QtWidgets.QVBoxLayout()
+            lights_VERTLAY.setObjectName("lights_VERTLAY")
+            defaultLights_GRIDLAY.addWidget(self.spotLight_BTN, 0, 0, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.pointLight_BTN, 0, 1, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.areaLight_BTN, 0, 2, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.directionalLight_BTN, 0, 3, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.ambientLight_BTN, 0, 4, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.volumeLight_BTN, 0, 5, 1, 1)
+            lights_VERTLAY.addLayout(defaultLights_GRIDLAY)
+            rendererLights_GRIDLAY.addWidget(self.aiAreaLight_BTN, 0, 0, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiSkyDomeLight_BTN, 0, 1, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiMeshLight_BTN, 0, 2, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiPhotometricLight_BTN, 0, 3, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiLightPortal_BTN, 0, 4, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiPhysicalSky_BTN, 0, 5, 1, 1)
+            lights_VERTLAY.addLayout(rendererLights_GRIDLAY)
+            mainLayout.addLayout(lights_VERTLAY)
+            spacerItem3 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight, 
+                                        QtWidgets.QSizePolicy.Expanding,
+                                        QtWidgets.QSizePolicy.Minimum)
+            mainLayout.addItem(spacerItem3)
+        else:
+            tools_GRIDLAY.addWidget(self.simpleIsolate_BTN, 0, 0, 1, 1)
+            tools_GRIDLAY.addWidget(self.lookThru_BTN, 0, 1, 1, 1)
+            tools_GRIDLAY.addWidget(self.aimLight_BTN, 1, 0, 1, 1)
+            tools_GRIDLAY.addWidget(self.specularConstrain_BTN, 2, 0, 1, 1)
+            tools_GRIDLAY.addWidget(self.cleanUpCams_BTN, 2, 1, 1, 1)
+            tools_GRIDLAY.addWidget(self.lightsManager_BTN, 3, 0, 1, 2)
+            tools_GRIDLAY.addWidget(self.alignLight_BTN, 1, 1, 1, 1)
+            mainLayout.addLayout(tools_GRIDLAY)
+            spacerWidth = 20
+            spacerHeight = 8
+            spacerItem1 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight,
+                                QtWidgets.QSizePolicy.Minimum,
+                                QtWidgets.QSizePolicy.Expanding)
+            mainLayout.addItem(spacerItem1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot01_BTN, 0, 0, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot02_BTN, 0, 1, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot03_BTN, 0, 2, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot04_BTN, 1, 0, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot05_BTN, 1, 1, 1, 1)
+            lgtVis_GRIDLAY.addWidget(self.visSnapshot06_BTN, 1, 2, 1, 1)
+            mainLayout.addLayout(lgtVis_GRIDLAY)
+            spacerItem2 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight,
+                                        QtWidgets.QSizePolicy.Minimum,
+                                        QtWidgets.QSizePolicy.Expanding)
+            mainLayout.addItem(spacerItem2)
+            defaultLights_GRIDLAY.addWidget(self.spotLight_BTN, 0, 0, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.pointLight_BTN, 0, 1, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.areaLight_BTN, 0, 2, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.directionalLight_BTN, 1, 0, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.ambientLight_BTN, 1, 1, 1, 1)
+            defaultLights_GRIDLAY.addWidget(self.volumeLight_BTN, 1, 2, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiAreaLight_BTN, 0, 0, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiSkyDomeLight_BTN, 0, 1, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiMeshLight_BTN, 0, 2, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiPhotometricLight_BTN, 1, 0, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiLightPortal_BTN, 1, 1, 1, 1)
+            rendererLights_GRIDLAY.addWidget(self.aiPhysicalSky_BTN, 1, 2, 1, 1)
+            mainLayout.addLayout(defaultLights_GRIDLAY)
+            mainLayout.addLayout(rendererLights_GRIDLAY)
+            spacerItem3 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight,
+                                        QtWidgets.QSizePolicy.Minimum,
+                                        QtWidgets.QSizePolicy.Expanding)
+            mainLayout.addItem(spacerItem3)
+
+        tools_GRIDLAY.setAlignment(self.lightsManager_BTN, QtCore.Qt.AlignCenter)
+        mainLayout.addWidget(self.config_BTN)
+        mainLayout.setAlignment(self.config_BTN, QtCore.Qt.AlignCenter)
+        self.setLayout(mainLayout)
+        
+        QtCore.QMetaObject.connectSlotsByName(self)
+    
+    def createButtons(self):
+        '''TODO: Should the light creation buttons be responsibility of factories?'''
         toolsBtnSize = QtCore.QSize(32, 32)
+        visBtnSize = QtCore.QSize(14, 14)
+        lightsBtnSize = QtCore.QSize(16, 16)
+        configBtnSize = QtCore.QSize(20, 20)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                             QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
+        # TOOLS
         self.simpleIsolate_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.lookThru_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.aimLight_BTN = QtWidgets.QPushButton(self.centralwidget)
@@ -110,40 +220,8 @@ class MiniToolsDialog(QtWidgets.QDialog):
         self.lightsManager_BTN.setMaximumSize(toolsBtnSize)
         self.lightsManager_BTN.setText("")
         self.lightsManager_BTN.setObjectName("lightsManager_BTN")
-        if mainLayout.objectName() == 'horizontalLayout':
-            tools_GRIDLAY.addWidget(self.simpleIsolate_BTN, 0, 0, 1, 1)
-            tools_GRIDLAY.addWidget(self.lookThru_BTN, 0, 1, 1, 1)
-            tools_GRIDLAY.addWidget(self.aimLight_BTN, 0, 2, 1, 1)
-            tools_GRIDLAY.addWidget(self.specularConstrain_BTN, 0, 3, 2, 1)
-            tools_GRIDLAY.addWidget(self.cleanUpCams_BTN, 1, 0, 1, 1)
-            tools_GRIDLAY.addWidget(self.lightsManager_BTN, 1, 1, 1, 1)
-            tools_GRIDLAY.addWidget(self.alignLight_BTN, 1, 2, 1, 1)
-        else:
-            tools_GRIDLAY.addWidget(self.simpleIsolate_BTN, 0, 0, 1, 1)
-            tools_GRIDLAY.addWidget(self.lookThru_BTN, 0, 1, 1, 1)
-            tools_GRIDLAY.addWidget(self.aimLight_BTN, 1, 0, 1, 1)
-            tools_GRIDLAY.addWidget(self.specularConstrain_BTN, 2, 0, 1, 1)
-            tools_GRIDLAY.addWidget(self.cleanUpCams_BTN, 2, 1, 1, 1)
-            tools_GRIDLAY.addWidget(self.lightsManager_BTN, 3, 0, 1, 2)
-            tools_GRIDLAY.addWidget(self.alignLight_BTN, 1, 1, 1, 1)
-        tools_GRIDLAY.setAlignment(self.lightsManager_BTN, QtCore.Qt.AlignCenter)
-        mainLayout.addLayout(tools_GRIDLAY)
-        if mainLayout.objectName() == 'horizontalLayout':
-            spacerWidth = 8
-            spacerHeight = 20
-            spacerItem1 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight, 
-                                QtWidgets.QSizePolicy.Expanding,
-                                QtWidgets.QSizePolicy.Minimum)
-        else:
-            spacerWidth = 20
-            spacerHeight = 8
-            spacerItem1 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight,
-                                QtWidgets.QSizePolicy.Minimum,
-                                QtWidgets.QSizePolicy.Expanding)
-        mainLayout.addItem(spacerItem1)
-        lgtVis_GRIDLAY = QtWidgets.QGridLayout()
-        lgtVis_GRIDLAY.setObjectName("lgtVis_GRIDLAY")
-        visBtnSize = QtCore.QSize(14, 14)
+
+        # LIGHT VIS SNAPSHOTS
         self.visSnapshot01_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.visSnapshot02_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.visSnapshot03_BTN = QtWidgets.QPushButton(self.centralwidget)
@@ -180,36 +258,8 @@ class MiniToolsDialog(QtWidgets.QDialog):
         self.visSnapshot06_BTN.setMinimumSize(visBtnSize)
         self.visSnapshot06_BTN.setMaximumSize(visBtnSize)
         self.visSnapshot06_BTN.setText("")
-        if mainLayout.objectName() == 'horizontalLayout':
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot01_BTN, 0, 0, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot02_BTN, 0, 1, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot03_BTN, 2, 1, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot04_BTN, 1, 0, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot05_BTN, 1, 1, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot06_BTN, 2, 0, 1, 1)
-        else:
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot01_BTN, 0, 0, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot02_BTN, 0, 1, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot03_BTN, 0, 2, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot04_BTN, 1, 0, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot05_BTN, 1, 1, 1, 1)
-            lgtVis_GRIDLAY.addWidget(self.visSnapshot06_BTN, 1, 2, 1, 1)
-        mainLayout.addLayout(lgtVis_GRIDLAY)
-        if mainLayout.objectName() == 'horizontalLayout':
-            spacerItem2 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight, 
-                                        QtWidgets.QSizePolicy.Expanding,
-                                        QtWidgets.QSizePolicy.Minimum)
-        else:
-            spacerItem2 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight,
-                                        QtWidgets.QSizePolicy.Minimum,
-                                        QtWidgets.QSizePolicy.Expanding)
-        mainLayout.addItem(spacerItem2)
-        if mainLayout.objectName() == 'horizontalLayout':
-            lights_VERTLAY = QtWidgets.QVBoxLayout()
-            lights_VERTLAY.setObjectName("lights_VERTLAY")
-        defaultLights_GRIDLAY = QtWidgets.QGridLayout()
-        defaultLights_GRIDLAY.setObjectName("defaultLights_GRIDLAY")
-        lightsBtnSize = QtCore.QSize(16, 16)
+
+        # DEFAULT LIGHTS
         self.spotLight_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.pointLight_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.areaLight_BTN = QtWidgets.QPushButton(self.centralwidget)
@@ -246,24 +296,8 @@ class MiniToolsDialog(QtWidgets.QDialog):
         self.volumeLight_BTN.setMaximumSize(QtCore.QSize(16, 16))
         self.volumeLight_BTN.setText("")
         self.volumeLight_BTN.setObjectName("volumeLight_BTN")
-        if mainLayout.objectName() == 'horizontalLayout':
-            defaultLights_GRIDLAY.addWidget(self.spotLight_BTN, 0, 0, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.pointLight_BTN, 0, 1, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.areaLight_BTN, 0, 2, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.directionalLight_BTN, 0, 3, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.ambientLight_BTN, 0, 4, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.volumeLight_BTN, 0, 5, 1, 1)
-        else:
-            defaultLights_GRIDLAY.addWidget(self.spotLight_BTN, 0, 0, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.pointLight_BTN, 0, 1, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.areaLight_BTN, 0, 2, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.directionalLight_BTN, 1, 0, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.ambientLight_BTN, 1, 1, 1, 1)
-            defaultLights_GRIDLAY.addWidget(self.volumeLight_BTN, 1, 2, 1, 1)
-        if mainLayout.objectName() == 'horizontalLayout':
-            lights_VERTLAY.addLayout(defaultLights_GRIDLAY)
-        rendererLights_GRIDLAY = QtWidgets.QGridLayout()
-        rendererLights_GRIDLAY.setObjectName("rendererLights_GRIDLAY")
+
+        # MTOA LIGHTS
         self.aiAreaLight_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.aiSkyDomeLight_BTN = QtWidgets.QPushButton(self.centralwidget)
         self.aiMeshLight_BTN = QtWidgets.QPushButton(self.centralwidget)
@@ -300,46 +334,12 @@ class MiniToolsDialog(QtWidgets.QDialog):
         self.aiPhysicalSky_BTN.setMaximumSize(lightsBtnSize)
         self.aiPhysicalSky_BTN.setText("")
         self.aiPhysicalSky_BTN.setObjectName("aiPhysicalSky_BTN")
-        if mainLayout.objectName() == 'horizontalLayout':
-            rendererLights_GRIDLAY.addWidget(self.aiAreaLight_BTN, 0, 0, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiSkyDomeLight_BTN, 0, 1, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiMeshLight_BTN, 0, 2, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiPhotometricLight_BTN, 0, 3, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiLightPortal_BTN, 0, 4, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiPhysicalSky_BTN, 0, 5, 1, 1)
-        else:
-            rendererLights_GRIDLAY.addWidget(self.aiAreaLight_BTN, 0, 0, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiSkyDomeLight_BTN, 0, 1, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiMeshLight_BTN, 0, 2, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiPhotometricLight_BTN, 1, 0, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiLightPortal_BTN, 1, 1, 1, 1)
-            rendererLights_GRIDLAY.addWidget(self.aiPhysicalSky_BTN, 1, 2, 1, 1)
-        if mainLayout.objectName() == 'horizontalLayout':
-            lights_VERTLAY.addLayout(rendererLights_GRIDLAY)
-            mainLayout.addLayout(lights_VERTLAY)
-        else:
-            mainLayout.addLayout(defaultLights_GRIDLAY)
-            mainLayout.addLayout(rendererLights_GRIDLAY)
-        if mainLayout.objectName() == 'horizontalLayout':
-            spacerItem3 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight, 
-                                        QtWidgets.QSizePolicy.Expanding,
-                                        QtWidgets.QSizePolicy.Minimum)
-        else:
-            spacerItem3 = QtWidgets.QSpacerItem(spacerWidth, spacerHeight,
-                                        QtWidgets.QSizePolicy.Minimum,
-                                        QtWidgets.QSizePolicy.Expanding)
-        mainLayout.addItem(spacerItem3)
+
+        #CONFIG
         self.config_BTN = QtWidgets.QPushButton(self.centralwidget)
-        self.config_BTN.setMaximumSize(QtCore.QSize(20, 20))
+        self.config_BTN.setMaximumSize(configBtnSize)
         self.config_BTN.setText("")
         self.config_BTN.setObjectName("config_BTN")
-        mainLayout.addWidget(self.config_BTN)
-        mainLayout.setAlignment(self.config_BTN, QtCore.Qt.AlignCenter)
-        #self.setCentralWidget(self.centralwidget)
-        self.setLayout(mainLayout)
-        
-        QtCore.QMetaObject.connectSlotsByName(self)
-        
         
     def loadHorizontal(self):
         horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
