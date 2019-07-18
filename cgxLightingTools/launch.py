66b97a029e177e71dc2603d1d8f84573ffb83770
Chris Granados- Xian <chris.granados@xiancg.com>
2019-07-18 00:29:48 -0300

    Added launch module

---
+++ cgxLightingTools/launch.py
@@ -0,0 +1,14 @@
+'''
+Created on July 18, 2019
+
+@author: Chris Granados - Xian
+@contact: chris.granados@xiancg.com http://www.chrisgranados.com/
+'''
+from __future__ import absolute_import
+import cgxLightingTools.scripts.gui.minitools as minitools
+
+def launch():
+    minitools.load()
+
+if __name__ == '__main__' or 'eclipsePython' in __name__:
+    launch()
