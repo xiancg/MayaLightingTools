5ffdbc4fecfd9e8a14a7ac33908c71a888fa9947
Chris Granados- Xian <chris.granados@xiancg.com>
2019-07-20 21:07:52 -0300

    Tweaked to do's

--- cgxLightingTools/scripts/gui/minitools.py
+++ cgxLightingTools/scripts/gui/minitools.py
@@ -3,7 +3,6 @@ Created on July 10, 2019
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
-TODO: Add clipping planes default values to config
 TODO: Add conform to naming tool
 TODO: Add separators to naming library
 TODO: Create GUI for naming library
