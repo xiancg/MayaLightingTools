554c8e37922483b7c979f6c9988c9f344e06dca5
Chris Granados- Xian <chris.granados@xiancg.com>
2019-08-04 02:08:30 -0300

    Changed AOV creation method. Used mtoa.aovs so all hooks and stuff is correctly managed to work with Render Setup

--- cgxLightingTools/scripts/toolbox/lightsFactory/post_functions.py
+++ cgxLightingTools/scripts/toolbox/lightsFactory/post_functions.py
@@ -3,48 +3,39 @@ Created on July 30, 2019
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
+NOTE: DO NOT delete any function, even if you're not using it
 '''
 
 from __future__ import absolute_import
+
+#PLACE YOUR IMPORTS HERE
 import maya.cmds as mc
+from mtoa.core import createOptions
+import mtoa.aovs as aovs
 
 
 def postLightCreation(shapeNode, *args, **kwargs):
     '''Place here all custom stuff you want to do with the created light node'''
     transform = mc.listRelatives(shapeNode, parent=True)[0]
     mc.setAttr(shapeNode + '.aiAov', 'LG_' + transform, type='string')
-    aovNode = mc.createNode("aiAOV", name='RGBA_LG_' + transform)
+    newAOVName = 'RGBA_LG_' + transform
+    aovNode = aovs.AOVInterface().addAOV(newAOVName).node
     mc.setAttr(aovNode + ".name", 'RGBA_LG_' + transform, type="string")
     mc.setAttr(aovNode + ".type", 6)
     mc.setAttr(aovNode + ".enabled", True)
-    try:
-        mc.connectAttr("defaultArnoldFilter.message", aovNode + ".outputs[0].filter", force=True)
-        mc.connectAttr("defaultArnoldDriver.message", aovNode + ".outputs[0].driver", force=True)
-        mc.connectAttr(aovNode + ".message", "defaultArnoldRenderOptions.aovList",
-                        nextAvailable=True, force=True)
-    except RuntimeError:
-        raise RuntimeError('Could not make connections from AOV to default filter, driver and aovList for {}'.format(aovNode))
-    finally:
-        mc.select(shapeNode, replace=True)
+    mc.select(shapeNode, replace=True)
 
 def postLightDuplicate(shapeNode, *args, **kwargs):
     '''Place here all custom stuff you want to do with the duplicated light node'''
     if not kwargs.get('keepAOVSetup'):
         transform = mc.listRelatives(shapeNode, parent=True)[0]
         mc.setAttr(shapeNode + '.aiAov', 'LG_' + transform, type='string')
-        aovNode = mc.createNode("aiAOV", name='RGBA_LG_' + transform)
+        newAOVName = 'RGBA_LG_' + transform
+        aovNode = aovs.AOVInterface().addAOV(newAOVName).node
         mc.setAttr(aovNode + ".name", 'RGBA_LG_' + transform, type="string")
         mc.setAttr(aovNode + ".type", 6)
         mc.setAttr(aovNode + ".enabled", True)
-        try:
-            mc.connectAttr("defaultArnoldFilter.message", aovNode + ".outputs[0].filter", force=True)
-            mc.connectAttr("defaultArnoldDriver.message", aovNode + ".outputs[0].driver", force=True)
-            mc.connectAttr(aovNode + ".message", "defaultArnoldRenderOptions.aovList",
-                            nextAvailable=True, force=True)
-        except RuntimeError:
-            raise RuntimeError('Could not make connections from AOV to default filter, driver and aovList for {}'.format(aovNode))
-        finally:
-            mc.select(shapeNode, replace=True)
+        mc.select(shapeNode, replace=True)
 
 def postLightRename(shapeNode, *args, **kwargs):
     '''Place here all custom stuff you want to do with the renamed light node'''
