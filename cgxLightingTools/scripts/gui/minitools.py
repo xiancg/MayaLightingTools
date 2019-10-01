852be64ea623e421840e0be7bb0450629fbb7826
xiancg <chris.granados@xiancg.com>
2019-10-01 14:06:40 -0300

    Updated to do's

--- cgxLightingTools/scripts/gui/minitools.py
+++ cgxLightingTools/scripts/gui/minitools.py
@@ -1,6 +1,7 @@
 '''
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
+TODO: Assign hotkey for deleting (Ctrl+Del?)
 TODO: Move rename, duplicate and delete closer to the creation buttons.
 TODO: Options to create lights aligned with selection with some default offset
 TODO: Add separators to naming library
