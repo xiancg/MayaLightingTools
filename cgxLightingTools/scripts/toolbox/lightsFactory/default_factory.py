4d3fd74ee0abd1e363b9ddbed8e92647f6cb7ba1
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-06 13:58:10 -0300

    python3 refactor

--- cgxLightingTools/scripts/toolbox/lightsFactory/default_factory.py
+++ cgxLightingTools/scripts/toolbox/lightsFactory/default_factory.py
@@ -1,11 +1,14 @@
 '''
 Created on July 3, 2019
+Python 3 refactor, Jun 6, 2022
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
-TODO: Implement attributes on default and arnold. This will require 
+TODO: Implement attributes on default and arnold. This will require
 a lot of work to do it automatically, use config file preset for now.
 '''
+
+import contextlib
 import os
 import json
 import pkgutil
@@ -20,7 +23,7 @@ class LightsFactory(object):
     def __init__(self):
         super(LightsFactory, self).__init__()
         self.lightNodeTypes = tools.getDefaultLightNodes()
-        repoPath = os.path.join(os.path.dirname(os.path.abspath(naming.__file__)),'cfg')
+        repoPath = os.path.join(os.path.dirname(os.path.abspath(naming.__file__)), 'cfg')
         os.environ['NAMING_REPO'] = repoPath
         naming.loadSession()
         self.naming = naming
@@ -29,7 +32,7 @@ class LightsFactory(object):
             config = json.load(fp)
         self._lightAttrs = config
         self.post_fn = self._initPostFunctions()
-    
+
     def createLight(self, lightNodeType, lightName, *args, **kwargs):
         preCreationSelection = mc.ls(sl=True, long=True)
         if len(preCreationSelection) < 1:
@@ -49,32 +52,33 @@ class LightsFactory(object):
             initNode = mc.createNode('volumeLight')
             transform = mc.listRelatives(initNode, parent=True, fullPath=True)[0]
             result = mc.rename(transform, lightName)
-            shapeNode = mc.listRelatives(result, shapes=True, 
-                                        noIntermediate=True, type='light')[0]
+            shapeNode = mc.listRelatives(result, shapes=True,
+                                         noIntermediate=True, type='light')[0]
         elif lightNodeType == 'areaLight':
             initNode = mc.createNode('areaLight')
             transform = mc.listRelatives(initNode, parent=True, fullPath=True)[0]
             result = mc.rename(transform, lightName)
             shapeNode = mc.listRelatives(result, shapes=True,
-                                        noIntermediate=True, type='light')[0]
+                                         noIntermediate=True, type='light')[0]
         else:
             return None
         if shapeNode:
             transformNode = mc.listRelatives(shapeNode, parent=True)[0]
             self.alignLight(transformNode, preCreationSelection)
             tools.setDefaultAttrs(shapeNode)
-            
+
             try:
                 self.post_fn.postLightCreation(transformNode, shapeNode, *args, **kwargs)
-            except:
-                tools.logger.exception("Post light creation function not executed due to exceptions")
+            except Exception:
+                tools.logger.exception(
+                    "Post light creation function not executed due to exceptions")
             finally:
                 return transformNode, shapeNode
-        else: 
+        else:
             return None
-    
+
     def duplicateLight(self, lightNode, withInputs=False, withNodes=False, *args, **kwargs):
-        objTransform, objShape= tools.getTransformAndShape(lightNode)
+        objTransform, objShape = tools.getTransformAndShape(lightNode)
         transformNode = str()
         shapeNode = str()
         if mc.nodeType(objShape) in tools.getLightNodesList():
@@ -82,32 +86,33 @@ class LightsFactory(object):
             if parsedName is not None:
                 lightName = self.buildName(**parsedName)
                 if withInputs and not withNodes:
-                    transformNode = mc.duplicate(objTransform, name= lightName, ic= True)[0]
-                elif withInputs and withNodes:
-                    transformNode = mc.duplicate(objTransform, name= lightName, ic= True, un=True)[0]
+                    transformNode = mc.duplicate(objTransform, name=lightName, ic=True)[0]
+                elif withInputs:
+                    transformNode = mc.duplicate(objTransform, name=lightName, ic=True, un=True)[0]
                 else:
-                    transformNode = mc.duplicate(objTransform, name= lightName)[0]
+                    transformNode = mc.duplicate(objTransform, name=lightName)[0]
+            elif withInputs and not withNodes:
+                transformNode = mc.duplicate(objTransform, ic=True)[0]
+            elif withInputs:
+                transformNode = mc.duplicate(objTransform, ic=True, un=True)[0]
             else:
-                if withInputs and not withNodes:
-                    transformNode = mc.duplicate(objTransform, ic= True)[0]
-                elif withInputs and withNodes:
-                    transformNode = mc.duplicate(objTransform, ic= True, un=True)[0]
-                else:
-                    transformNode = mc.duplicate(objTransform)[0]
-            shapeNode = mc.listRelatives(transformNode, shapes=True, noIntermediate=True, fullPath=True)[0]
+                transformNode = mc.duplicate(objTransform)[0]
+            shapeNode = mc.listRelatives(transformNode, shapes=True,
+                                         noIntermediate=True, fullPath=True)[0]
         else:
-            tools.logger.info('Only lights accepted. {} is {}'.format(lightNode, mc.nodeType(lightNode)))
+            tools.logger.info(f'Only lights accepted. {lightNode} is {mc.nodeType(lightNode)}')
+
             return None
-        
+
         try:
             self.post_fn.postLightDuplicate(transformNode, shapeNode, *args, **kwargs)
-        except:
+        except Exception:
             tools.logger.exception("Post light duplicate function not executed due to exceptions")
         finally:
             return transformNode, shapeNode
-    
+
     def renameLight(self, lightNode, lightName, *args, **kwargs):
-        objTransform, objShape= tools.getTransformAndShape(lightNode)
+        objTransform, objShape = tools.getTransformAndShape(lightNode)
         parsedName = self.naming.parse(lightName)
         finalLightName = self.buildName(**parsedName)
         result = mc.rename(objTransform, finalLightName)
@@ -115,88 +120,75 @@ class LightsFactory(object):
 
         try:
             self.post_fn.postLightRename(result, shapeNode, *args, **kwargs)
-        except:
+        except Exception:
             tools.logger.exception("Post light rename function not executed due to exceptions")
         finally:
             return result
-    
+
     def deleteLight(self, lightNode, lightName, *args, **kwargs):
-        objTransform, objShape= tools.getTransformAndShape(lightNode)
+        objTransform, objShape = tools.getTransformAndShape(lightNode)
         success = False
-        try:
+        with contextlib.suppress(Exception):
             mc.delete(objTransform)
             success = True
-        except:
-            pass
-
         try:
             self.post_fn.postLightDelete(objTransform, objShape, *args, **kwargs)
-        except:
+        except Exception:
             tools.logger.exception("Post light delete function not executed due to exceptions")
         finally:
             return success
-    
+
     def alignLight(self, lightTransform, objTransform):
-        if objTransform != None:
+        if objTransform is not None:
             if mc.objExists(objTransform) and mc.nodeType(objTransform) == 'transform':
                 selList = [lightTransform, objTransform]
                 tools.alignLightToObject(selList)
-                tools.logger.warning('Created light {} aligned to {}.'.format(lightTransform, objTransform))
-            else:
-                tools.logger.warning('No transform selection found to align {}, created at default position.'.format(lightTransform))
-    
-    def parseOldNameByTokens(self, lightNode):
-        objTransform, objShape= tools.getTransformAndShape(lightNode)
-        nameSplit = objTransform.split('_')
-        result = None
-        if len(nameSplit) == len(self.naming.getActiveRule().fields):
-            if mc.nodeType(objShape) in self.lightNodeTypes:
-                result = self.naming.parse(objTransform)
+                tools.logger.warning(f'Created light {lightTransform} aligned to {objTransform}.')
 
-        return result
-    
+            else:
+                tools.logger.warning(
+                    f'No transform selection found to align {lightTransform}, created at default position.')
+
+    def parseOldNameByTokens(self, lightNode):
+        objTransform, objShape = tools.getTransformAndShape(lightNode)
+        nameSplit = objTransform.split('_')
+        if len(nameSplit) == len(self.naming.getActiveRule().fields) and mc.nodeType(objShape) in self.lightNodeTypes:
+            return self.naming.parse(objTransform)
+        else:
+            return None
+
     def buildName(self, *args, **kwargs):
         '''Recursive method to check if the light name
         is already in use and modify it accordingly if it is'''
         lightName = self.naming.solve(*args, **kwargs)
-        if not mc.objExists(lightName):
-			return lightName
-        else:
-            #Number might be passed as arg or kwarg
-            argsList= list(args)
+        if mc.objExists(lightName):
+            # Number might be passed as arg or kwarg
+            argsList = list(args)
             for i, arg in enumerate(argsList):
                 if isinstance(arg, int):
                     argsList[i] += 1
             args = tuple(argsList)
-            for k, v in kwargs.iteritems():
+            for k, v in kwargs.items():
                 if isinstance(v, int):
                     kwargs[k] = v + 1
 
             lightName = self.buildName(*args, **kwargs)
-            return lightName
-    
+        return lightName
+
     def _initPostFunctions(self):
         currentRenderer = tools.getCurrentRenderPlugin()
         renderers = tools.getRenderEngines()
         if renderers is not None:
             rendererNames = renderers.keys()
             postFuncPath = os.path.dirname(post_functions.__file__)
-            postFuncs = [name for _, name, _ in pkgutil.iter_modules([postFuncPath]) if name.endswith('post_functions')]
+            postFuncs = [name for _, name, _ in pkgutil.iter_modules(
+                [postFuncPath]) if name.endswith('post_functions')]
             for postFunc in postFuncs:
                 if postFunc.rsplit('_')[0] in rendererNames and postFunc.rsplit('_')[0] == currentRenderer:
-                    for name, obj in inspect.getmembers(eval('post_functions.{}'.format(postFunc))):
-                        if inspect.isclass(obj) and name == ('PostFunctions_{}'.format(currentRenderer)):
-                            postFuncObj = eval('post_functions.{}.{}()'.format(postFunc, name))
+                    for name, obj in inspect.getmembers(eval(f'post_functions.{postFunc}')):
+                        if inspect.isclass(obj) and name == f'PostFunctions_{currentRenderer}':
+                            postFuncObj = eval(f'post_functions.{postFunc}.{name}()')
                             return postFuncObj
             # Returns default post functions if no match for current renderer found
             postFuncObj = post_functions.default_post_functions.PostFunctions_default()
             return postFuncObj
-
-# --------------------------------------------------------
-#  Main
-# --------------------------------------------------------
-def main():
-    pass
-
-if __name__ == '__main__' or 'eclipsePython' in __name__:
-    main()
