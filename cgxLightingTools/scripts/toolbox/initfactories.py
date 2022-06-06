24d66378e50f768e3a27f3e7d04f8995cd452e0b
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-06 12:35:43 -0300

    python3 refactor, reformat

--- cgxLightingTools/scripts/toolbox/initfactories.py
+++ cgxLightingTools/scripts/toolbox/initfactories.py
@@ -1,11 +1,11 @@
 '''
 Created on Aug 30, 2019
+Python 3 refactor, Jun 6, 2022
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
 '''
 
-from __future__ import absolute_import
 import os
 import pkgutil
 import inspect
@@ -16,29 +16,18 @@ import cgxLightingTools.scripts.toolbox.lightsFactory as lightsFactory
 
 def init_factories():
     defaultFactory = lightsFactory.default_factory.LightsFactory()
-    result = {'default':defaultFactory}
+    result = {'default': defaultFactory}
     renderers = tools.getRenderEngines()
     if renderers is not None:
         rendererNames = renderers.keys()
         factoryPath = os.path.dirname(lightsFactory.__file__)
         factories = [name for _, name, _ in pkgutil.iter_modules([factoryPath])
-                        if name.endswith('factory')]
+                     if name.endswith('factory')]
         for factory in factories:
             if factory.rsplit('_')[0] in rendererNames:
-                for name, obj in inspect.getmembers(eval('lightsFactory.{}'.format(factory))):
+                for name, obj in inspect.getmembers(eval(f'lightsFactory.{factory}')):
                     if inspect.isclass(obj) and name != 'LightsFactory':
-                        factoryObj = eval('lightsFactory.{}.{}()'.format(factory, name))
+                        factoryObj = eval(f'lightsFactory.{factory}.{name}()')
                         result[factory.split('_')[0]] = factoryObj
                         break
     return result
-
-
-# --------------------------------------------------------
-#  Main
-# --------------------------------------------------------
-def main():
-    pass
-
-
-if __name__ == '__main__' or 'eclipsePython' in __name__:
-    main()
