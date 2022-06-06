fe33c9080cc718f149becf35fa04c21d3f1175c5
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-06 12:35:25 -0300

    python3 refactor, reformat

--- cgxLightingTools/launch.py
+++ cgxLightingTools/launch.py
@@ -1,14 +1,16 @@
 '''
 Created on July 18, 2019
+Python 3 refactor, Jun 6, 2022
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
 '''
-from __future__ import absolute_import
 import cgxLightingTools.scripts.gui.minitools as minitools
+
 
 def launch():
     minitools.load()
+
 
 if __name__ == '__main__' or 'eclipsePython' in __name__:
     launch()
