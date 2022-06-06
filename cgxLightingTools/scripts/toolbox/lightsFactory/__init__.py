ae99e049be49d8638c72ae530abc8399eb49642b
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-06 13:58:04 -0300

    python3 refactor

--- cgxLightingTools/scripts/toolbox/lightsFactory/__init__.py
+++ cgxLightingTools/scripts/toolbox/lightsFactory/__init__.py
@@ -1,5 +1,6 @@
 '''
 Created on July 15, 2019
+Python 3 refactor, Jun 6, 2022
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
@@ -7,10 +8,13 @@ Imports all factories from this package to the package namespace,
 so they can be used as for example: import lightsFactory.default_factory
 '''
 
+
 def _import_factories():
     import os.path
     import pkgutil
     factoryPath = os.path.dirname(__file__)
     return [name for _, name, _ in pkgutil.iter_modules([factoryPath])]
+
+
 __all__ = _import_factories()
 from . import *
