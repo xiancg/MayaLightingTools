cc5c370518c900bd8e7494013eaf9a99ec1a7ba9
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-06 13:02:16 -0300

    python 3 migration

--- cgxLightingTools/scripts/gui/mayaWindow.py
+++ cgxLightingTools/scripts/gui/mayaWindow.py
@@ -1,11 +1,11 @@
 '''
 Created on July 12, 2019
+Python 3 refactor, Jun 6, 2022
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
 PySide defs- by Jason Parks and Nathan Horne
 '''
-from __future__ import absolute_import
 import maya.OpenMayaUI as mui
 from PySide2 import QtWidgets
 from shiboken2 import wrapInstance
@@ -14,15 +14,4 @@ from shiboken2 import wrapInstance
 def getMayaWindow():
     '''Get the Maya main window as a QMainWindow instance'''
     ptr = mui.MQtUtil.mainWindow()
-    return wrapInstance(long(ptr), QtWidgets.QWidget)
-
-
-# --------------------------------------------------------
-#  Main
-# --------------------------------------------------------
-def main():
-    pass
-
-
-if __name__=="__main__":
-    main()
+    return wrapInstance(int(ptr), QtWidgets.QWidget)
