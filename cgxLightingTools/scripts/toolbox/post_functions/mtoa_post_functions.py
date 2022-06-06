41859ae348a75abb20452b741543737e05256ac2
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-06 12:35:52 -0300

    python3 refactor, reformat

--- cgxLightingTools/scripts/toolbox/post_functions/mtoa_post_functions.py
+++ cgxLightingTools/scripts/toolbox/post_functions/mtoa_post_functions.py
@@ -1,12 +1,16 @@
 '''
+Python 3 refactor, Jun 6, 2022
+
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
 '''
-from __future__ import absolute_import
 
+
+import contextlib
 import maya.cmds as mc
 import mtoa.aovs as aovs
 from cgxLightingTools.scripts.toolbox.post_functions.default_post_functions import PostFunctions_default
+
 
 class PostFunctions_mtoa(PostFunctions_default):
     def __init__(self):
@@ -14,43 +18,41 @@ class PostFunctions_mtoa(PostFunctions_default):
 
     def postLightCreation(self, transformNode, shapeNode, *args, **kwargs):
         '''Place here all custom stuff you want to do with the created light node'''
-        mc.setAttr(shapeNode + '.aiAov', 'LG_' + transformNode, type='string')
-        newAOVName = 'RGBA_LG_' + transformNode
+        mc.setAttr(f'{shapeNode}.aiAov', f'LG_{transformNode}', type='string')
+        newAOVName = f'RGBA_LG_{transformNode}'
         aovNode = aovs.AOVInterface().addAOV(newAOVName).node
-        mc.setAttr(aovNode + ".name", 'RGBA_LG_' + transformNode, type="string")
-        mc.setAttr(aovNode + ".type", 6)
-        mc.setAttr(aovNode + ".enabled", True)
+        mc.setAttr(f"{aovNode}.name", f'RGBA_LG_{transformNode}', type="string")
+        mc.setAttr(f"{aovNode}.type", 6)
+        mc.setAttr(f"{aovNode}.enabled", True)
         mc.select(shapeNode, replace=True)
 
     def postLightDuplicate(self, transformNode, shapeNode, *args, **kwargs):
         '''Place here all custom stuff you want to do with the duplicated light node'''
         if not kwargs.get('keepAOVSetup'):
-            mc.setAttr(shapeNode + '.aiAov', 'LG_' + transformNode, type='string')
-            newAOVName = 'RGBA_LG_' + transformNode
+            mc.setAttr(f'{shapeNode}.aiAov', f'LG_{transformNode}', type='string')
+            newAOVName = f'RGBA_LG_{transformNode}'
             aovNode = aovs.AOVInterface().addAOV(newAOVName).node
-            mc.setAttr(aovNode + ".name", 'RGBA_LG_' + transformNode, type="string")
-            mc.setAttr(aovNode + ".type", 6)
-            mc.setAttr(aovNode + ".enabled", True)
+            mc.setAttr(f"{aovNode}.name", f'RGBA_LG_{transformNode}', type="string")
+            mc.setAttr(f"{aovNode}.type", 6)
+            mc.setAttr(f"{aovNode}.enabled", True)
             mc.select(shapeNode, replace=True)
 
     def postLightRename(self, transformNode, shapeNode, *args, **kwargs):
         '''Place here all custom stuff you want to do with the renamed light node'''
-        oldLightGroup = mc.getAttr(shapeNode + '.aiAov')
-        oldAOV = 'aiAOV_RGBA_' + oldLightGroup
-        mc.setAttr(shapeNode + '.aiAov', 'LG_' + transformNode, type='string')
+        oldLightGroup = mc.getAttr(f'{shapeNode}.aiAov')
+        oldAOV = f'aiAOV_RGBA_{oldLightGroup}'
+        mc.setAttr(f'{shapeNode}.aiAov', f'LG_{transformNode}', type='string')
         if mc.objExists(oldAOV):
-            aovNode = mc.rename(oldAOV, 'RGBA_LG_' + transformNode)
-            mc.setAttr(aovNode + ".name", 'RGBA_LG_' + transformNode, type="string")
+            aovNode = mc.rename(oldAOV, f'RGBA_LG_{transformNode}')
+            mc.setAttr(f"{aovNode}.name", f'RGBA_LG_{transformNode}', type="string")
         mc.select(shapeNode, replace=True)
-    
+
     def postLightDelete(self, transformNode, shapeNode, *args, **kwargs):
         '''Place here all custom stuff you want to do with the deleted light node.
            Keep in mind the node doesn't exist anymore, you're just operating
            with the name.
         '''
-        oldAOV = 'aiAOV_RGBA_LG_' + transformNode
+        oldAOV = f'aiAOV_RGBA_LG_{transformNode}'
         if mc.objExists(oldAOV):
-            try:
+            with contextlib.suppress(Exception):
                 mc.delete(oldAOV)
-            except:
-                pass
