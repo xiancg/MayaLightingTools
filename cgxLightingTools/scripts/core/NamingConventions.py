bb25347640d4cbe16b5ce1221b20df014bf3b923
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-06 13:57:59 -0300

    python3 refactor

--- cgxLightingTools/scripts/core/NamingConventions.py
+++ cgxLightingTools/scripts/core/NamingConventions.py
@@ -1,19 +1,21 @@
 '''
 Created on July 4, 2019
+Python 3 refactor, Jun 6, 2022
 
 @author: Chris Granados - Xian
 @contact: chris.granados@xiancg.com http://www.chrisgranados.com/
 Heavily based upon the work of Cesar Saez https://www.cesarsaez.me
 TODO: Add separator functionality
 '''
-from __future__ import absolute_import
+
 import copy
-import os 
+import os
 import json
 
 NAMING_REPO_ENV = "NAMING_REPO"
 _rules = {'_active': None}
-_tokens = dict()
+_tokens = {}
+
 
 class Serializable(object):
     def data(self):
@@ -30,57 +32,53 @@ class Serializable(object):
         if data.get("_Serializable_version") is not None:
             del data["_Serializable_version"]
 
-        if cls.__name__ == 'Rule':
-            this = cls(None, [None])
-        else:
-            this = cls(None)
+        this = cls(None, [None]) if cls.__name__ == 'Rule' else cls(None)
         this.__dict__.update(data)
         return this
+
 
 class Token(Serializable):
     def __init__(self, name):
         super(Token, self).__init__()
         self._name = name
         self._default = None
-        self._options = dict()
-    
+        self._options = {}
+
     def addOption(self, key, value):
         self._options[key] = value
 
     def solve(self, name=None):
         '''Solve for abbreviation given a certain name. Ex: center could return C'''
-        if name is None:
-            return self.default
-        return self._options.get(name)
-    
+        return self.default if name is None else self._options.get(name)
+
     def parse(self, value):
         '''Get metatada (origin) for given value in name. Ex: L could return left'''
-        for k, v in self._options.iteritems():
+        for k, v in self._options.items():
             if v == value:
                 return k
-    
+
     @property
     def required(self):
         return self.default is None
-    
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, n):
         self._name = n
-    
+
     @property
     def default(self):
         if self._default is None and len(self._options) >= 1:
             self._default = self._options.values()[0]
         return self._default
-    
+
     @default.setter
     def default(self, d):
         self._default = d
-    
+
     @property
     def options(self):
         return copy.deepcopy(self._options)
@@ -94,48 +92,47 @@ class TokenNumber(Token):
     def __init__(self, name):
         super(TokenNumber, self).__init__(name)
         self._isNumber = True
-        
+
     def solve(self, number):
         '''Solve for number with given padding parameter.
             Ex: 1 could return 001 with padding 3'''
         numberStr = str(number).zfill(self.padding)
-        return '{}{}{}'.format(self.prefix, numberStr, self.suffix)
-    
+        return f'{self.prefix}{numberStr}{self.suffix}'
+
     def parse(self, value):
         '''Get metatada (number) for given value in name. Ex: v0025 could return 25'''
         if value.isdigit():
             return int(value)
-        else:
-            prefixIndex = 0
-            for each in value[::1]:
-                if each.isdigit() and prefixIndex == 0:
-                    prefixIndex = -1
-                    break
-                if not each.isdigit():
-                    prefixIndex += 1
-                else:
-                    break
-            suffixIndex = 0
-            for each in value[::-1]:
-                if each.isdigit() and suffixIndex == 0:
-                    suffixIndex = -1
-                    break
-                if not each.isdigit():
-                    suffixIndex += 1
-                else:
-                    break
-            
-            if prefixIndex == -1 and suffixIndex >= 0:
-                return int(value[:-suffixIndex])
-            elif prefixIndex >= 0 and suffixIndex == -1:
-                return int(value[prefixIndex:])
-            elif prefixIndex >= 0 and suffixIndex >= 0:
-                return int(value[prefixIndex:-suffixIndex])
-    
+        prefixIndex = 0
+        for each in value[::1]:
+            if each.isdigit() and prefixIndex == 0:
+                prefixIndex = -1
+                break
+            if each.isdigit():
+                break
+            else:
+                prefixIndex += 1
+        suffixIndex = 0
+        for each in value[::-1]:
+            if each.isdigit() and suffixIndex == 0:
+                suffixIndex = -1
+                break
+            if not each.isdigit():
+                suffixIndex += 1
+            else:
+                break
+
+        if prefixIndex == -1 and suffixIndex >= 0:
+            return int(value[:-suffixIndex])
+        elif prefixIndex >= 0 and suffixIndex == -1:
+            return int(value[prefixIndex:])
+        elif prefixIndex >= 0 and suffixIndex >= 0:
+            return int(value[prefixIndex:-suffixIndex])
+
     @property
     def padding(self):
         return self._options['padding']
-    
+
     @padding.setter
     def padding(self, p):
         if p <= 0:
@@ -145,7 +142,7 @@ class TokenNumber(Token):
     @property
     def prefix(self):
         return self._options['prefix']
-    
+
     @prefix.setter
     def prefix(self, p):
         self._options['prefix'] = p
@@ -153,20 +150,20 @@ class TokenNumber(Token):
     @property
     def suffix(self):
         return self._options['suffix']
-    
+
     @suffix.setter
     def suffix(self, s):
         self._options['suffix'] = s
-    
+
     @property
     def default(self):
         self._default = 1
         return self._default
-    
+
     @property
     def required(self):
         return True
-    
+
     @property
     def isNumber(self):
         return True
@@ -176,29 +173,26 @@ class Rule(Serializable):
     def __init__(self, name, fields):
         super(Rule, self).__init__()
         self.name = name
-        self._fields = list()
+        self._fields = []
         self.addFields(fields)
-    
+
     def addFields(self, tokenNames):
         self._fields.extend(tokenNames)
         return True
-    
+
     def solve(self, **values):
         '''Build the name string with given values and return it'''
         return self._pattern.format(**values)
-    
+
     def parse(self, name):
         '''Build and return dictionary with keys as tokens and values as given names'''
-        retval = dict()
+        retval = {}
         splitName = name.split('_')
         for i, f in enumerate(self.fields):
             namePart = splitName[i]
             token = _tokens[f]
             if token.required:
-                if token.isNumber:
-                    retval[f] = token.parse(namePart)
-                else:
-                    retval[f] = namePart
+                retval[f] = token.parse(namePart) if token.isNumber else namePart
                 continue
             retval[f] = token.parse(namePart)
         return retval
@@ -214,7 +208,7 @@ class Rule(Serializable):
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, n):
         self._name = n
@@ -276,16 +270,16 @@ def loadRule(filepath):
     try:
         with open(filepath) as fp:
             data = json.load(fp)
-    except:
+    except Exception:
         return False
     rule = Rule.fromData(data)
     _rules[rule.name] = rule
     return True
 
 
-def addToken(name,**kwargs):
+def addToken(name, **kwargs):
     token = Token(name)
-    for k, v in kwargs.iteritems():
+    for k, v in kwargs.items():
         if k == "default":
             token.default = v
             continue
@@ -309,6 +303,7 @@ def resetTokens():
     _tokens.clear()
     return True
 
+
 def getToken(name):
     return _tokens.get(name)
 
@@ -328,7 +323,7 @@ def loadToken(filepath):
     try:
         with open(filepath) as fp:
             data = json.load(fp)
-    except:
+    except Exception:
         return False
     if data.get("_Serializable_classname") == 'TokenNumber':
         token = TokenNumber.fromData(data)
@@ -349,11 +344,11 @@ def addTokenNumber(name, prefix='', padding=3, suffix=''):
 
 def parse(name):
     rule = getActiveRule()
-    return rule.parse(name)  
+    return rule.parse(name)
 
 
 def solve(*args, **kwargs):
-    values = dict()
+    values = {}
     rule = getActiveRule()
     i = 0
     for f in rule.fields:
@@ -388,20 +383,20 @@ def saveSession(repo=None):
     if not os.path.exists(repo):
         os.mkdir(repo)
     # tokens and rules
-    for name, token in _tokens.iteritems():
-        filepath = os.path.join(repo, name + ".token")
+    for name, token in _tokens.items():
+        filepath = os.path.join(repo, f"{name}.token")
         saveToken(name, filepath)
-    for name, rule in _rules.iteritems():
+    for name, rule in _rules.items():
         if not isinstance(rule, Rule):
             continue
-        filepath = os.path.join(repo, name + ".rule")
+        filepath = os.path.join(repo, f"{name}.rule")
         saveRule(name, filepath)
     # extra configuration
     active = getActiveRule()
     config = {"setActiveRule": active.name if active else None}
     filepath = os.path.join(repo, "naming.conf")
     with open(filepath, "w") as fp:
-        json.dump(config, fp, indent = 4)
+        json.dump(config, fp, indent=4)
     return True
 
 
@@ -420,6 +415,6 @@ def loadSession(repo=None):
     if os.path.exists(filepath):
         with open(filepath) as fp:
             config = json.load(fp)
-        for k, v in config.iteritems():
-            globals()[k](v) #execute setActiveRule and sets it
+        for k, v in config.items():
+            globals()[k](v)  # execute setActiveRule and sets it
     return True
